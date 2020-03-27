6928e637abb7e4459ce903bc28c3eb886142c8ab
Brandon Reninger <brandonreninger@yahoo.com>
2020-03-27 16:26:46 -0600

    working getting _listService to work

--- app/Controllers/ListController.js
+++ app/Controllers/ListController.js
@@ -1,12 +1,15 @@
 import ListService from "../Services/ListService.js";
 import _store from "../store.js"
+import LISTSERVICE from "../Services/ListService.js";
 
 //TODO Don't forget to render to the screen after every data change.
+//ANCHOR Come back and get an "id" for this
 function _drawLists() {
   let template = ""
   let lists = _store.State.lists
 
   lists.forEach(list => template += list.Template)
+  document.getElementById("")
 
 }
 
@@ -18,8 +21,15 @@ export default class ListController {
   }
 
   //TODO: Your app will need the ability to create, and delete both lists and listItems
-  create() {
+  create(event) {
+    event.preventDefault()
+    let formData = event.target
 
+    let newList = {
+      title: formData.title.value
+    }
+
+    _listService.create(newList)
   }
 
 }
--- app/Models/List.js
+++ app/Models/List.js
@@ -5,12 +5,33 @@ import {
 export default class List {
   constructor(data) {
     //TODO Your constructor takes in a data object that should have the properties you need to create your list here is a freebie, it will set the id its provided, or if that is undefined it will create a new one (this is an alternative to object destructuring)
+    this.title = data.title
     this.id = data.id || generateId();
+    this.task = data.task
   }
   //Be sure to add the methods needed to create the view template for this model
   //For starting out, your tasks may be strings alone, but later you may wish to turn them into full objects, that will be up to you
 
   get Template() {
+    return /*html*/ `
+    <div class="col-4">
+        <div class="card" style="width: 18rem;">
+          <div class="card-header bg-dark text-white">
+            <h3>${this.title}</h3>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">${this.task}</li>
+            <div class="input-group mb-3 mt-3">
+            <label for="task"></label>
+              <input type="text" name="task" class="form-control" placeholder="Add task...">
+              <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">Add</button>
+              </div>
+            </div>
+          </ul>
+        </div>
+      </div>
+    `
 
   }
 
